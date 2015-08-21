13080692c77eae569ff28bc972b6f6e17a903144
Atli <atligud13@ru.is>
2015-08-21 15:36:41 +0000

    added a new activity displaying the user input

--- Activity1/MyFirstApp/app/src/main/java/com/example/atli/myfirstapp/DisplayMessageActivity.java
+++ Activity1/MyFirstApp/app/src/main/java/com/example/atli/myfirstapp/DisplayMessageActivity.java
@@ -5,23 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MyActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.atli.myfirstapp.MESSAGE";
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+        //Get the message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setText(message);
+        textView.setTextSize(40);
+
+        //Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
@@ -37,13 +39,5 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
